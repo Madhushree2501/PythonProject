05284ce0dc2196a22910080ee798b7aa781f877f
Madhushree2501 <madhushreemahadev25@gmail.com>
2026-02-12 20:38:07 +0530

    Assignment1 result

---
+++ Assignment1_Task2.py
@@ -0,0 +1,5 @@
+#Create the personalized greeting
+fName= input("Enter your first name: ")
+lName= input("Enter your last name: ")
+name = fName + " " + lName + "!"
+print("Hello,",name,"Welcome to Python program.")
