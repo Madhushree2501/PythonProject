95bb2ff346b7c252b2318cd5cc3023d77a0c326b
Madhushree2501 <madhushreemahadev25@gmail.com>
2026-02-25 20:55:13 +0530

    Assignment 2 result and readme added

--- Assignment1_Task2.py
+++ Assignment1_Task2.py
@@ -1,5 +1,10 @@
 #Create the personalized greeting
+#Take the First name and last name from user
 fName= input("Enter your first name: ")
 lName= input("Enter your last name: ")
+#Storing both First and last name in variable
 name = fName + " " + lName + "!"
 print("Hello,",name,"Welcome to Python program.")
+
+#Using format string
+print(f"Hello, {fName} {lName}! Welcome to Python program.")
--- Assignment2_Task1.py
+++ Assignment2_Task1.py
@@ -1,5 +1,7 @@
 #Check if number is Even or Odd
+#Take the number from the user
 num = int(input("Enter a number: "))
+#Using Arithmetic operator modulus to get remainder and checking it is zero
 if num % 2 == 0:
     print(f'{num} is an even number.')
 else:
